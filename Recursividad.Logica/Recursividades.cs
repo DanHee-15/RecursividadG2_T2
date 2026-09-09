09301d15b91dc374e0e6b78dc9139a68adc4edcc
DanHee-15 <25030021@itesa.edu.mx>
2026-09-09 14:23:46 -0600

    Versión_02:Factorial recursivo

---
+++ Recursividad.Logica/Recursividades.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividad.Logica
+{
+    public class Recursividades
+    {
+        public int CalcularFactorail(int num)
+        {
+            //caso base o condicion de salida
+
+            if(num == 1)
+            {
+                return 1;
+            }
+
+            //Caso General
+            return num = num * CalcularFactorail(num - 1);
+        }
+    }
+}
---
+++ Recursividad.UI.Consola/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Recursividad.Logica;
+
+Console.WriteLine("Metodos recursivos");
+Console.WriteLine("\n\nCalcular Factorial de 5");
+
+
+Recursividades oRecursividades = new Recursividades();
+int resultado = oRecursividades.CalcularFactorail(5);
+
+
+Console.WriteLine("Factorial de 5: " + resultado);
+
